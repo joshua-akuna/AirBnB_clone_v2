c9a739321d32daa8d0e6f95ee4df459cf6ed513e
Akuna Joshua <akunajoshua@gmail.com>
2023-10-05 03:24:02 +0100

    0-setup_web_static.sh file updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,27 +3,26 @@
 
 if ! command -V nginx &> /dev/null
 then
-	# sudo apt-get update
-	# sudo apt-get install nginx
-	echo "installing" # TO BE DELETED
+	sudo apt-get update
+	sudo apt-get install nginx
 fi
 
-mkdir -p data/web_static/releases/test/
-mkdir -p data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
 echo "<html>
-	<head></head>
+	<head>
+	</head>
 	<body>
 		Holberton School
 	</body>
-</html>" > data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html >& /dev/null
 
-if [[ -L "/data/web_static/current" ]]
-then
-	echo "SYMLINK EXISTS"
-fi
+sudo ln -sf  /data/web_static/releases/test /data/web_static/current
 
-ln -sf  data/web_static/releases/test data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 
-ls -l data # TO DELETE
-chown -R ubuntu:ubuntu data
-ls -l data # TO DELETE
+config_file="/etc/nginx/sites-available/default"
+replacement="server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}"
+sudo sed -i "s/server_name _;/$replacement/" "$config_file"
+
+sudo service nginx restart
