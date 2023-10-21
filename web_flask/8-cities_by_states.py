1467d426bc594b4fe06a2d08b46d1a3e8facc105
Akuna Joshua <akunajoshua@gmail.com>
2023-10-21 06:14:09 +0100

    web_flask/8-cities_by_states.py file commit

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+'''
+
+import sys
+
+sys.path.append('../')
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    return render_template('8-cities_by_states.html',
+            states=storage.all("State").values())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=1)
